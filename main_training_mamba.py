716201f560945d41a880618056b898d623f59fe6
kcirred <16872435+kcirred@users.noreply.github.com>
2025-04-15 17:47:29 -0400

    [main_training] improve param find

--- main_training_mamba.py
+++ main_training_mamba.py
@@ -112,16 +112,17 @@ def main(**kwargs):
     # )
     params_with_decay = []
     params_without_decay = []
+
     for name, param in model.named_parameters():
         print(f'{name=}')
-        if 'A_log' in name or 'D' in name or 'dt_bias' in name:
+        suff = name.split('.')[-1]
+        if 'A_log' in suff or 'D' in suff or 'dt_bias' in suff:
             params_without_decay.append(param)
         else:
             params_with_decay.append(param)
 
-
-    print(f'{params_with_decay=}')
-    print(f'{params_without_decay=}')
+    print(f'{len(params_with_decay)=}')
+    print(f'{len(params_without_decay)=}')
 
     optimizer = optim.AdamW(
         [
